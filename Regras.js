49bff0371d3536443383a64ac9d19193e69fd08f
ADRIANO VARALDA <adriano.varalda@senacsp.edu.br>
2019-08-08 20:37:56 -0300

    Conhecendo regras

---
+++ Regras.js
@@ -0,0 +1,33 @@
+// Exemplo p/ nao se fazer
+// let 1nome =''.
+
+// exemplo para se fazer
+// string
+let nome = 'varalda';
+
+// tipo inteiro
+let idade = 47
+
+// tipo decimal
+let peso = 83.40
+
+//tipo boleano
+
+let visualizado = true;  
+
+// criar constante
+const nome1= 'varalda';
+
+// exibir informacoes
+console.log('este é o console.log');
+
+console.log('exibindo variaveis');
+nome = 'Adriano'
+console.log('nome :' + nome);
+
+console.log('nome const:' + nome)
+
+console.log( 'nome const: + nome2')
+
+console.log('nome: ' + nome + ' idade:'+ idade);
+console.log( `${nome} idade ${idade}`)
